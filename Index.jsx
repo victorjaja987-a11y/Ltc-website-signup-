ada5470120915d7a160b6441284e7f1ad5567cd3
victorjaja987-a11y <victorjaja987@gmail.com>
2026-01-03 20:00:48 +0100

    Create Index.jsx

---
+++ Index.jsx
@@ -0,0 +1,15 @@
+import HeroSection from "./HeroSection";
+import SignupForm from "./SignupForm";
+import Footer from "./Footer";
+
+const Index = () => {
+  return (
+    <main className="min-h-screen bg-background">
+      <HeroSection />
+      <SignupForm />
+      <Footer />
+    </main>
+  );
+};
+
+export default Index;
